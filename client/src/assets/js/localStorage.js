8f8190a2ad4854e1c0094b574397343322097dbf
GonzalezNacho <nacho.ldm63@gmail.com>
2024-04-16 19:44:00 -0300

    borrando archivos sin uso

--- client/src/assets/js/localStorage.js
+++
@@ -1,9 +0,0 @@
-export const loginToStorage = ({user, isLogin}) => {
-    window.localStorage.setItem('user', user)
-    window.localStorage.setItem('isLogin', isLogin)
-}
-
-export const logoutToStorage = () => {
-    window.localStorage.removeItem('user')
-    window.localStorage.removeItem('isLogin')
-}
