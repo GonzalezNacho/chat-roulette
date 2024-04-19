69348f0a121d5617ce2f9a765be0c78d0d16da46
GonzalezNacho <nacho.ldm63@gmail.com>
2024-04-19 15:01:43 -0300

    cambios en el css

--- client/src/routes/Bienvenida.jsx
+++ client/src/routes/Bienvenida.jsx
@@ -1,18 +1,18 @@
 export function Bienvenido() {
     
     return (
-        <>
+        <section className="content">
             <h1>Bienvenido a Chat Roulette</h1>    
-            <section className="conteiners-divs">
+            <div className="conteiners-divs">
                 <div>
-                    <h1>Chat Roulette</h1>
-                    <p>Chat Roulette es una excelente manera de conocer nuevos amigos. Cuando usa chat random se le empareja con otra persona para hablar uno a uno. Si lo prefiere, puede agregar sus intereses y sera emparejado al azar con alguien que selecciono algunos de sus mismos intereses</p>
+                    <h2>Chat Roulette</h2>
+                    <p>Chat Roulette es una plataforma de chat aleatorio que conecta a usuarios de todo el mundo en conversaciones en tiempo real. Al ingresar al sitio, los usuarios son emparejados de manera aleatoria con otros participantes para tener conversaciones</p>
                 </div>
                 <div>
-                    <h1>Seguridad</h1>
-                    <p>Para ayudarlo a mantenerse seguro , los chats son anonimos a menos que le diga a alguien quien es usted (no recomendado) , y puede detener un chat en cualquier momento.Consulte nuestros <a href="">terminos de servicio</a> y <a href="">pautas de comunidad</a> para obtener mas informacion sobre lo que se debe y no se debe hacer al usar chat random.LOS USUARIOS SON LOS UNICOS RESPONSABLES DE SU COMPORTAMIENTO mientras usan chat Random.</p>
+                    <h2>Seguridad</h2>
+                    <p>Para su seguridad, solo se mostrará su nombre de usuario y puede terminar el chat cuando lo desee. Revise nuestros <a href="">términos de servicio</a> y <a href="">pautas de comunidad</a> para más detalles sobre el uso de Chat Random. LOS USUARIOS SON LOS UNICOS RESPONSABLES DE SU COMPORTAMIENTO en Chat Rulette</p>
                 </div>
-            </section>
-        </>
+            </div>
+        </section>
     )
 }
