7b8575cfcbfdba8ca2cefe6175bee748d3a8d61d
GonzalezNacho <nacho.ldm63@gmail.com>
2024-06-03 12:25:13 -0300

    sin bootstrap

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -7,15 +7,11 @@ export function Messages ({messages, nickname ='Yo'}) {
             {
                 messages.map((message, index) => {
                     return (
-                        <div key={index} className={`d-flex p-3 ${message.from === nickname? 'justify-content-end': 'justify-content-start'}`}>
-                            <div className={`card mb-3 border-1 ${message.from === nickname? 'bg-success bg-opacity-25': 'bg-light' }`}>
-                                <div className='card-body'>
+                        <div key={index} className={`messages ${message.from === nickname? 'my-message': 'other-message'}`}>
                                 { nickname != 'Yo'
-                                    ? <small className='text-muted'>{message.from}: {message.messages}</small>
-                                    : <small className=''>{message.from}: {message.body}</small>
+                                    ? <small>{message.from}: {message.messages}</small>
+                                    : <small>{message.from}: {message.body}</small>
                                 }
-                                </div>  
-                            </div>
                         </div>
                     )
                 })
